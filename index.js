e5d036e73b1362d89ea4a304dfd8570d9e612780
samiulbasirfahim360 <samiulbasirfahim360@hotmail.com>
2022-05-15 19:30:04 +0600

    user list jwt token admin add remove appoinment list available treatment done

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express")
 const cors = require("cors")
+const jwt = require("jsonwebtoken")
 const { MongoClient, ServerApiVersion } = require("mongodb")
 require("dotenv").config()
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+const verifyToken = (req, res, next) => {
+	const token = req?.headers?.authorization?.split(" ")[1]
+	jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+		if (err) {
+			res.status(403).send({ message: "unauthorized" })
+		} else {
+			if (decoded.email === req.headers.email) {
+				req.decoded = decoded
+				next()
+			} else {
+				res.status(403).send({ message: "forbidden" })
+			}
+		}
+	})
+}
 
 const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASSWORD}@cluster0.fnda1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const client = new MongoClient(uri, {
@@ -23,12 +40,70 @@ const run = async () => {
 		const bookingCollection = client
 			.db("doctorsPortal")
 			.collection("bookings")
+		const userCollection = client.db("doctorsPortal").collection("users")
 
-		app.get("/treatment", async (req, res) => {
+		app.get("/user", verifyToken, async (req, res) => {
+			res.send(await userCollection.find({}).toArray())
+		})
+
+		app.get("/user/:email", async (req, res) => {
+			res.send(await userCollection.findOne({ email: req.params.email }))
+		})
+
+		app.put("/admin/:email", async (req, res) => {
+			const query = { email: req.params.email }
+			const updatedDoc = {
+				$set: {
+					role: "admin",
+				},
+			}
+			res.send(await userCollection.updateOne(query, updatedDoc))
+		})
+		app.put("/admin-remove/:email", verifyToken,  async (req, res) => {
+			const query = { email: req.params.email }
+			const updatedDoc = {
+				$set: {
+					role: "users",
+				},
+			}
+			res.send(await userCollection.updateOne(query, updatedDoc))
+		})
+
+		app.put("/user", async (req, res) => {
+			const query = { email: req.headers.email }
+			const options = { upsert: true }
+			const exist = await userCollection.findOne(query)
+			let updatedDoc
+			if (exist) {
+				updatedDoc = {
+					$set: { ...req.body },
+				}
+			} else {
+				updatedDoc = {
+					$set: { ...req.body, role: "users" },
+				}
+			}
+
+			const result = await userCollection.updateOne(
+				query,
+				updatedDoc,
+				options
+			)
+			result.token = jwt.sign(
+				{ email: req.body.email },
+				process.env.SECRET_KEY,
+				{
+					expiresIn: "1hr",
+				}
+			)
+			res.send(result)
+		})
+
+		app.get("/treatment", verifyToken, async (req, res) => {
 			res.send(await treatmentCollection.find({}).toArray())
 		})
 
-		app.get("/available", async (req, res) => {
+		app.get("/available", verifyToken,  async (req, res) => {
 			const treatments = await treatmentCollection.find({}).toArray()
 			const booking = await bookingCollection
 				.find({ date: req.query.date })
@@ -45,11 +120,11 @@ const run = async () => {
 			res.send(treatments)
 		})
 
-		app.post("/booking", async (req, res) => {
+		app.post("/booking", verifyToken, async (req, res) => {
 			res.send(await bookingCollection.insertOne(req.body))
 		})
 
-		app.get("/my-appointments", async (req, res) => {
+		app.get("/my-appointments", verifyToken, async (req, res) => {
 			res.send(
 				await bookingCollection
 					.find({ email: req.query.email })
